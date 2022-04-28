c1f7708a2c063be85879c537b67ecd7094200e4d
Kavindu Chamith <kavindu.chamith@pagero.com>
2022-04-28 10:06:23 +0530

    flip animation when order changes

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,6 +1,8 @@
 <script>
-// import { onDestroy, onMount } from 'svelte';
-
+  // these only works on plain html elements, not custom elements
+  // transitions controls how element enter or delete from dom
+  import { fade, scale} from 'svelte/transition';
+  import {flip} from 'svelte/animate';
   import PollStore from '../store/PollStore.js';
   import PollDetails from "./PollDetails.svelte";
 
@@ -32,11 +34,17 @@
 </script>
 <!-- we directly access pollstore, so it will automatically 
 unmounted, no need hooks -->
+
+ <!-- forward the event to parent, bcz polldetails not nested in app.svelte -->
+  <!-- <PollDetails {poll} on:vote /> no need to forward anymore bcz store -->
+  <!-- local means animate only individual element removed from dom -->
+    
+<!-- animate:flip -> animate when the order of "each" block changes -->
 <div class="poll-list">
   {#each $PollStore as poll (poll.id)}
-    <!-- forward the event to parent, bcz polldetails not nested in app.svelte -->
-    <!-- <PollDetails {poll} on:vote /> no need to forward anymore bcz store -->
-    <PollDetails {poll} />
+   <div in:fade out:scale|local animate:flip={{duration: 500}}>
+      <PollDetails {poll}/>
+    </div>
   {/each}
 </div>
 
